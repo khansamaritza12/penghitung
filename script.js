87be80f7056184978b16cc26d5f303241ad996cf
khansamaritza12 <khansamaritza8@gmail.com>
2023-11-21 09:48:41 +0700

    Kalkulator

---
+++ script.js
@@ -0,0 +1,41 @@
+
+// Insert angka ke textview
+function insert(num){
+	document.form.textarea.value = document.form.textarea.value + num;
+}
+
+// Fungsi Hitung
+function equal(){
+	var hasil = document.form.textarea.value;
+	document.form.textarea.value = eval(hasil);
+}
+
+// Clean
+function clean(){
+	document.form.textarea.value = "";
+}
+
+// Delete
+function back(){
+	var hasil = document.form.textarea.value;
+	document.form.textarea.value = hasil.substring(0,hasil.length-1);
+}
+
+function ValidateInput(value) {
+    let last_input = input.slice(-1);
+    let operators = ["+","-","*","+"];
+
+    if (value == "."&& last_input == ".") {
+        return false;
+    }
+    if (operators.includes(value)){
+        if (operators.includes(last_input)){
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+return true
+}
